6e07a057279b81f6cd8a74c6b186b93f1f9390ef
LP <972885245@qq.com>
2026-06-14 16:27:20 +0800

    Revert "add bgm"
    This reverts commit 967796568eaabd2d2d6c7cf045f70e2960c9d390.

--- love/file/functions.js
+++ love/file/functions.js
@@ -30,7 +30,6 @@ $(window).resize(function() {
 				$ele.html(str.substring(0, progress) + (progress & 1 ? '_' : ''));
 				if (progress >= str.length) {
 					clearInterval(timer);
-					$ele.html(str);
 				}
 			}, 75);
 		});
@@ -56,6 +55,6 @@ function timeElapse(date){
 	if (seconds < 10) {
 		seconds = "0" + seconds;
 	}
-	var result = "第 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒";
+	var result = "第 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒"; 
 	$("#clock").html(result);
 }
